a33c5e02e63ad5445d8da1cd5f00c426168a5e78
stefi51 <stefi.morningstar@elfak.rs>
2020-08-26 19:18:10 +0200

    http client proba

--- AnalyticsMicroservice/Controllers/AnalyticsController.cs
+++ AnalyticsMicroservice/Controllers/AnalyticsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AnalyticsMicroservice.Models;
 using AnalyticsMicroservice.RefinedDataRepository;
@@ -14,14 +15,32 @@ namespace AnalyticsMicroservice.Controllers
     public class AnalyticsController : ControllerBase
     {
         private IRefinedDataRepository repository { get; set; }
+        private static  HttpClient _httpClient;
     
 
         public AnalyticsController(IRefinedDataRepository dataRepository)
         {
-            this.repository = dataRepository;         
-
+            this.repository = dataRepository;
+            _httpClient= new HttpClient();
+            Fja();
         }
 
+        static async void Fja()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("http://localhost:5002/api/devices");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
         [HttpGet("")]
         public ActionResult<IEnumerable<RefinedData>> GetData()
         {
